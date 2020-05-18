643f406253c57bdb06e9898a514d26f9f3a9a967
Moodrammer <mahmou.m.gody@gmail.com>
2020-05-18 13:01:59 +0200

    Add: Making cake phase finished

--- Final2/Final2.ino
+++ Final2/Final2.ino
@@ -5,6 +5,8 @@
 #define sugarValvePin 4
 #define flourValvePin 5
 
+#define stepsPerRevolution 100
+
 //Time it takes to go from 0 to 180 as its speed is 100ms / 60 degrees
 #define servoOpeningTime 300
 
@@ -12,13 +14,35 @@
 unsigned long currentMillis;
 unsigned long openingTime;
 unsigned long waitTime;
+
 //mixer motor variables
 int mixerCurrentStep = 0;
+int numRotations = 0;
+int mixerStepCount = 0;
+int mixerDelay = 1;
+int currentRotations;
 boolean isMixerOn = false;
+boolean countRotations = false;
+
 //valves control
 boolean isEggsAdded = false;
 boolean isEggValveOpened = false;
 boolean isEggValveClosed = true; 
+
+boolean isVanillaAdded = false;
+boolean isVanillaValveOpened = false;
+boolean isVanillaValveClosed = true;
+
+boolean isSugarAdded = false;
+boolean isSugarValveOpened = false;
+boolean isSugarValveClosed = true;
+
+int addingFlourTimes = 0;
+boolean isFlourAdded = false;
+boolean addFlourTime = false;
+boolean isFlourValveOpened = false;
+boolean isFlourValveClosed = true;
+boolean isFlourAddingStarted = false;
 
 void setup() {
   for(int i = 0; i <= 5; i++) pinMode(i, OUTPUT);
@@ -27,18 +51,74 @@ void setup() {
   closeValve(vanillaValvePin);
   closeValve(sugarValvePin);
   closeValve(flourValvePin);
+  delay(500);
 }
 
 void loop() {
   currentMillis = millis();
+  if(countRotations){
+    mixerStepCount++;
+    if(mixerStepCount == stepsPerRevolution){
+      numRotations++;
+      mixerStepCount = 0;  
+    }
+  }
+  
+  if(isMixerOn){
+    moveMotor(mixerAp, mixerBp, mixerCurrentStep, mixerDelay);
+    mixerCurrentStep ++; 
+  }
+  
   if(!isEggsAdded){
     waitTime = servoOpeningTime + 500; 
     addEggs();
   }
-  if(isMixerOn){
-    moveMotor(mixerAp, mixerBp, mixerCurrentStep, 1);
-    mixerCurrentStep ++; 
+
+  if(numRotations == 5 && !isVanillaAdded){
+    countRotations = false;
+    waitTime = servoOpeningTime + 100;
+    addVanilla();
   }
+
+  if(isVanillaAdded && !isSugarAdded){
+    if(numRotations - currentRotations == 4){
+      countRotations = false;
+      isMixerOn = false;
+      waitTime = servoOpeningTime + 200;
+      addSugar();
+    }
+  }
+
+  if(isSugarAdded && !isFlourAdded){
+    if(numRotations - currentRotations == 10){
+        countRotations = false;
+        isMixerOn = false;
+        //reduce the motor Speed
+        mixerDelay = 2;
+        currentRotations = numRotations;
+        addFlourTime = true;
+        addingFlourTimes++;
+        isFlourAddingStarted = true;
+    }
+
+    if(addFlourTime){
+      waitTime = servoOpeningTime + 100;
+      addFlour();
+    }
+    if(numRotations - currentRotations == 4 && isFlourAddingStarted){
+      isMixerOn = false;
+      countRotations = false;
+      currentRotations = numRotations;
+      if(addingFlourTimes != 3){
+        addingFlourTimes++;
+        addFlourTime = true;
+        isFlourValveOpened = false;
+        isFlourValveClosed = true;
+      }
+      else isFlourAdded = true;
+    }
+  }
+  
 
 }
 
@@ -93,6 +173,64 @@ void addEggs(){
       isEggValveClosed = true;
       isEggsAdded = true;
       isMixerOn = true;
+      countRotations = true;
+    }
+  }
+}
+
+void addVanilla(){
+  if(!isVanillaValveOpened){
+    openValve(vanillaValvePin);
+    isVanillaValveOpened = true;
+    isVanillaValveClosed = false;
+    openingTime = currentMillis;
+  }
+  else if(!isVanillaValveClosed){
+    if(currentMillis - openingTime >= waitTime){
+      if(currentMillis - openingTime >= servoOpeningTime){
+        countRotations = true;
+        currentRotations = numRotations;
+      }
+      closeValve(vanillaValvePin);
+      isVanillaValveClosed = true;
+      isVanillaAdded = true;
+    }
+  }
+}
+
+void addSugar(){
+  if(!isSugarValveOpened){
+    openValve(sugarValvePin);
+    isSugarValveOpened = true;
+    isSugarValveClosed = false;
+    openingTime = currentMillis;
+  }
+  else if(!isSugarValveClosed){
+    if(currentMillis - openingTime >= waitTime){
+      closeValve(sugarValvePin);
+      isSugarValveClosed = true;
+      isSugarAdded = true;
+      isMixerOn = true;
+      countRotations = true;
+      currentRotations = numRotations;
+    }
+  }
+}
+
+void addFlour(){
+  if(!isFlourValveOpened){
+    openValve(flourValvePin);
+    isFlourValveOpened = true;
+    isFlourValveClosed = false;
+    openingTime = currentMillis;
+  }
+  else if(!isFlourValveClosed){
+    if(currentMillis - openingTime >= waitTime){
+      closeValve(flourValvePin);
+      isFlourValveClosed = true;
+      isMixerOn = true;
+      countRotations = true;
+      addFlourTime = false;
     }
   }
 }
